12658d4a873e4703013970bb2c6fa2c68de7bfb3
wongself <wongself@gmail.com>
2020-05-16 13:27:06 +0800

    language

--- ner/urls.py
+++ ner/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+# from .application import entity
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('entity_query', entity.entity_query, name='entity_query'),
 ]
